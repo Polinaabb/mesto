947dad86f2c490d235d82f2b3458775cc73bad79
Poinaa_bb <olegbarannikov72@yandex.ru>
2023-06-15 17:56:22 +0500

    Ревью1

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,17 @@ let popupContainer = document.querySelector(".popup__container");
 let buttonClick = document.querySelector(".profile__edit-button");
 let profileName = document.querySelector(".profile__name");
 let profileDescription = document.querySelector(".profile__description");
-let buttonSave = document.querySelector(".popup__form-save");
-let buttonClose = document.querySelector(".popup__icon");
+let buttonSave = document.querySelector(".popup__container-save");
+let buttonClose = document.querySelector(".popup__close");
 let descriptionInput = document.querySelector(".popup__container_fieled");
-let nameInput = document.querySelector(".popup__form-name");
-let jobInput = document.querySelector(".popup__form-profession");
+let nameInput = document.querySelector(".popup__container-name");
+let jobInput = document.querySelector(".popup__container-job");
 
 
 buttonClick.addEventListener("click", function () {
     popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileDescription.textContent;
   });
 
 buttonClose.addEventListener("click", function () {
@@ -21,12 +23,8 @@ buttonClose.addEventListener("click", function () {
 function handleFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
-    profileProfession.textContent = jobInput.value;
+    profileDescription.textContent = jobInput.value;
     popup.classList.remove('popup_opened');
   };
-
-  buttonSave.addEventListener("click", function () {
-    popup.classList.remove('popup_opened');
-});
 
 popupContainer.addEventListener('submit', handleFormSubmit);
